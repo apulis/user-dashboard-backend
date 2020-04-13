86f37f91a03a6f3c02b98aef6e1e8a406d89226d
Han <xjhan1995@gmail.com>
2020-04-13 15:13:14 +0800

    增加 swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('User group example')
+    .setDescription('The user group API description')
+    .setVersion('1.0')
+    .addTag('user group')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/user-dashboard', app, document);
   await app.listen(5001);
 }
 bootstrap();
