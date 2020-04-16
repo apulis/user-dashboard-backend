83f802a6aea60ea0de8edf414e1b337d5ef3e47d
Han <xjhan1995@gmail.com>
2020-04-16 19:43:52 +0800

    添加 casbin 权限相关

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,28 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { typeOrmConfig } from './config/typeorm';
 import { GroupModule } from './group/group.module';
+import { PermissionModule } from './permission/permission.module';
+
+import { CasbinModule } from './common/authz';
+
 
 @Module({
   imports: [
+    CasbinModule.forRootAsync(
+      {
+        type: "mysql",
+        host: "localhost",
+        port: 3306,
+        username: "root",
+        password: "123456",
+        database: "user_group"
+      },
+      "src/common/authz/authz.model.conf"
+    ),
     TypeOrmModule.forRoot(typeOrmConfig as TypeOrmModuleOptions),
     UserModule,
     GroupModule,
+    PermissionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Enforcer } from "casbin";
+import {CASBIN_ENFORCER, CasbinService} from 'src/common/authz/index'
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(CASBIN_ENFORCER) private readonly enforcer: Enforcer,
+    private readonly casbinService: CasbinService
+  ) {}
   getHello(): object {
     return { a: 'b', c: 'b' };
   }
