9580b0d134566c4ef83507be7749914958a14453
Han <xjhan1995@gmail.com>
2020-04-26 20:29:35 +0800

    添加 group-swagger 的接口说明

--- src/group-user/group-user.dto.ts
+++ src/group-user/group-user.dto.ts
@@ -1,11 +1,20 @@
 import { IsArray, ArrayNotEmpty } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AddUsersToGroupDto {
   @ArrayNotEmpty()
   @IsArray()
+  @ApiProperty({
+    description: '用户id列表',
+    example: [1, 2, 3]
+  })
   userIds: number[];
   
   @ArrayNotEmpty()
   @IsArray()
+  @ApiProperty({
+    description: '用户组id列表',
+    example: [1, 2, 3]
+  })
   groupIds: number[];
 }
