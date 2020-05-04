d7c0277efc7b541bb181030893402de7203f667a
hanxianjie100 <xianjiezh@gmail.com>
2020-05-05 00:19:31 +0800

    添加初始化函数

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
+import 'initial/init-request';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
