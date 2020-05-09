a672419c8382c9218aefe6caef5eaade2f4d6d81
Han <xjhan1995@gmail.com>
2020-05-09 18:42:43 +0800

    备份

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PermissionService } from './permission/permission.service';
 import { RoleService } from './role/role.service';
 import { CasbinService } from './common/authz';
-import { initialPermissions } from 'mysql-init/init-permission';
 
 @Controller('/')
 export class AppController {
@@ -19,7 +18,6 @@ export class AppController {
     ])
     .then(async() => {
       casbinService.initRolePermissions();
-      console.log(await casbinService.getPermissionForRole(1))
     })
   }
 
