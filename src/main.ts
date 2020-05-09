af6ff02e8804dc89703fa53bb363a574a387bbf4
Han <xjhan1995@gmail.com>
2020-05-09 17:04:21 +0800

    添加限速中间件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
@@ -8,12 +9,19 @@ import 'initial/init-request';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   const options = new DocumentBuilder()
     .setTitle('User group example')
     .setDescription('The user group API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .addTag('user group')
     .build();
   const document = SwaggerModule.createDocument(app, options);
