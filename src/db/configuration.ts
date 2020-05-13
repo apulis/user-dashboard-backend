45f1718b1f32e389a5c282d4783dfceb3f6d4d39
Han <xjhan1995@gmail.com>
2020-05-13 20:12:47 +0800

    更新数据库配置读取设置

--- src/db/configuration.ts
+++ src/db/configuration.ts
@@ -1,20 +1,24 @@
 import 'dotenv/config';
 import * as dotenv from 'dotenv';
-const isTestEnvironment = process.env.NODE_ENV === 'test';
+import * as fs from 'fs';
+
 const isProductionEnvironment = process.env.NODE_ENV === 'production';
-console.log()
+
+console.log('process.env', process.env.DB_HOST)
+
+const envConfig = dotenv.parse(fs.readFileSync(process.env.NODE_ENV + '.env'));
+console.log('env', envConfig)
+
 export default {
   environment: process.env.NODE_ENV || 'development',
   typeorm: {
     db: {
-      host: process.env.DB_HOST || 'localhost',
-      type: process.env.DB_TYPE || 'mysql',
-      port: process.env.DB_PORT || 3306,
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || '123456',
-      database:
-        (isTestEnvironment ? process.env.TEST_DB_NAME : process.env.DB_NAME) ||
-        'user_group',
+      host: envConfig.DB_HOST || 'localhost',
+      type: envConfig.DB_TYPE || 'mysql',
+      port: envConfig.DB_PORT || 3306,
+      username: envConfig.DB_USERNAME || 'root',
+      password: envConfig.DB_PASSWORD || '123456',
+      database: envConfig.DB_NAME || 'user_group'
     },
     synchronize: !isProductionEnvironment,
     logging: !isProductionEnvironment,
