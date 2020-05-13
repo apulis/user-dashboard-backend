894b114e0efb0764235f3a28dd38b71ae03d01af
Han <xjhan1995@gmail.com>
2020-05-13 14:17:13 +0800

    增加cookie鉴权机制下 currentUser 的返回数据

--- src/guards/cookie.guard.ts
+++ src/guards/cookie.guard.ts
@@ -2,6 +2,7 @@ import { Injectable, CanActivate, ExecutionContext, Inject, UnauthorizedExceptio
 import { Observable, VirtualTimeScheduler } from 'rxjs';
 import { verify } from 'jsonwebtoken'
 import { ConfigService } from 'config/config.service';
+import { AuthService } from 'src/auth/auth.service';
 
 const validateRequest = (request: any, JWT_SECRET_KEY: string): boolean => {
   const { cookies } = request;
@@ -10,7 +11,7 @@ const validateRequest = (request: any, JWT_SECRET_KEY: string): boolean => {
     if (token) {
       try {
         const user = verify(token, JWT_SECRET_KEY);
-        request.user = user;
+        request.userId = (user as any).uid;
       } catch (err) {
         return false;
       }
@@ -25,14 +26,17 @@ const validateRequest = (request: any, JWT_SECRET_KEY: string): boolean => {
 @Injectable()
 export class CookieGuard implements CanActivate {
   constructor(
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
   ) {
   }
-  canActivate(
+  async canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ): Promise<boolean>{
     const request = context.switchToHttp().getRequest();
     if (validateRequest(request, this.configService.get('JWT_SECRET_KEY'))) {
+      const user = await this.authService.validateUser(request.userId);
+      request.user = user;
       return true
     };
     throw new UnauthorizedException();
--- src/open/open.module.ts
+++ src/open/open.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from 'config/config.service';
 import { OpenController } from './open.controller';
 import { OpenService } from './open.service';
+import { AuthService } from 'src/auth/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { GroupUser } from 'src/group-user/group-user.entity';
+import { GroupRole } from 'src/group-role/group-role.entity';
+import { UserRole } from 'src/user-role/user-role.entity';
+import { Role } from 'src/role/role.entity';
+import { InitCasbin } from 'src/common/authz/init-casbin';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, GroupUser, GroupRole, UserRole, Role]), InitCasbin],
   controllers: [OpenController],
-  providers: [OpenService, ConfigService],
+  providers: [OpenService, ConfigService, AuthService],
 })
 export class OpenModule {
 }
