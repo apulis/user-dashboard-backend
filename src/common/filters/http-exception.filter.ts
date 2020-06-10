74093d1dca0363c4906e468a25606df17e5442c2
Han <xjhan1995@gmail.com>
2020-06-10 17:10:44 +0800

    http 请求400报错

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -9,11 +9,13 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse();
     const request = ctx.getRequest();
     const status = exception.getStatus();
+    console.log(JSON.stringify(exception))
     response
       .status(status)
       .json({
         date: new Date().toLocaleDateString(),
         path: request.url,
+        message: exception.response.message,
       });
   }
 
