34e1d0ba2c6df57dfec3ba9fb6bd0285ebca3c7e
Han <xjhan1995@gmail.com>
2020-07-27 21:04:39 +0800

    add logger ip

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -8,7 +8,7 @@ export class LoggerMiddleware implements NestMiddleware {
     const code = res.statusCode; // 响应状态码
     next();
     // 组装日志信息
-    const logFormat = `Method: ${req.method} \n Request original url: ${req.originalUrl} \n IP: ${req.ip} \n Status code: ${code} \n`;
+    const logFormat = `Method: ${req.method} \n Request original url: ${req.originalUrl} \n IP: ${req.headers['X-Real-IP '] || req.headers['$remote_addr']} \n Status code: ${code} \n`;
     // 根据状态码，进行日志类型区分
     if (code >= 500) {
       Logger.error(logFormat);
@@ -28,7 +28,7 @@ export function logger(req: Request, res: Response, next: () => any) {
   const logFormat = ` >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     Request original url: ${req.originalUrl}
     Method: ${req.method}
-    IP: ${req.ip}
+    IP: ${req.headers['X-Real-IP '] || req.headers['$remote_addr']}
     Status code: ${code}
     Parmas: ${JSON.stringify(req.params)}
     Query: ${JSON.stringify(req.query)}
